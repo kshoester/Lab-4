8a91a513b5c7b397b763934d96513e0e0819d6b2
kshoester <156963490+kshoester@users.noreply.github.com>
2024-03-17 02:32:24 -0400

    edits

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const map = new mapboxgl.Map({
     zoom: 12 // starting zoom level
 });
 
+// Map controls
+map.addControl(new mapboxgl.NavigationControl(), 'bottom-left'); // add zoom + rotation controls
+map.addControl(new mapboxgl.FullscreenControl()); // add full screen control
 
 
 /*--------------------------------------------------------------------
@@ -102,7 +105,7 @@ map.on('load', () => {
 
     // set arguments for hexgrid function
     bbox = bboxcoords // instead of coordinates in [], use variable from earlier
-    cellSide = 0.5; // length of side of hexagons; also the radius 
+    cellSide = 0.3; // length of side of hexagons (3 km); also the radius 
     options = {units: 'kilometers'} // optional parameters
 
    // use turf transformScale method to fix points at edge of bounding box that are not covered by hexagons; define this variable before using it below
@@ -138,7 +141,7 @@ map.on('load', () => {
     console.log(maxcollis); // view max # of collisions in a polygon in console
 
 
-    // ADD TO MAP
+//------------ DATA VISUALIZATION ---------------//
     // collisions data
     map.addSource('pedcyc-collisions-data', { 
         'type': 'geojson',
@@ -149,7 +152,14 @@ map.on('load', () => {
         'type': 'circle',
         'source': 'pedcyc-collisions-data',
         'paint': {
-            'circle-radius': 2,
+            'circle-radius': [ // change marker size based on zoom
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                12, 2, // when zoom is 12 or less, 2px
+                12.5, 2.5, // 12-13, 2.5px
+                13, 3 // 13 or greater, 3px
+            ],
             'circle-color': '#ce1256'
         },
     });
@@ -189,7 +199,7 @@ map.on('load', () => {
                 10, '#8c6bb1', // if COUNT is 10-40
                 40, '#6e016b' // if COUNT is 40-69
             ],
-            'fill-opacity': 0.8,
+            'fill-opacity': 0.6,
             'fill-outline-color': 'black'
         },
     },
@@ -197,15 +207,81 @@ map.on('load', () => {
     
     );
 
+//------------ EVENTS ---------------//
+
+// add pop-up on click event; show pop up of collision count when hexagon is clicked
+map.on('click', 'hexgrid', (e) => { 
+    new mapboxgl.Popup() // declare new popup object on each click
+    .setLngLat(e.lngLat) // access long/lat based on mouse click location
+    .setHTML('<b>Number of Collisions: </b>' + e.features[0].properties.COUNT) // use click event properties to write popup text
+    .addTo(map);
+});
+
+// add pop-up on click event; show pop up of injury type when collision point is clicked
+map.on('mouseenter', 'pedcyc-collisions', () => {
+    map.getCanvas().style.cursor = 'pointer'; // change cursor to pointer when mouse is hovering collision point
+});
+map.on('mouseleave', 'pedcyc-collisions', () => { // change pointer to cursor when mouse is NOT hovering collision point
+    map.getCanvas().style.cursor = '';
+});
 
 
-
- 
-
-
+map.on('click', 'pedcyc-collisions', (e) => { 
+    new mapboxgl.Popup() // declare new popup object on each click
+    .setLngLat(e.lngLat) // access long/lat based on mouse click location
+    .setHTML('<b>Injury type: </b>' + e.features[0].properties.INJURY) // use click event properties to write popup text
+    .addTo(map);
+});
 
 });
  
-    
+// change map layer display based on check box using setLayoutProperty
+let layercheck = document.getElementById('layercheck');
 
-//});
+document.getElementById('layercheck').addEventListener('change', (e) => {
+    map.setLayoutProperty(
+        'pedcyc-collisions',
+        'visibility',
+        e.target.checked ? 'visible' : 'none'
+    );
+});
+
+//------------ LEGEND ---------------//
+//Declare array variables for labels and colours
+const legendlabels = [
+    '0-1',
+    '2-5',
+    '6-10',
+    '10-40',
+    '40-69'
+];
+
+const legendcolours = [
+    '#edf8fb',
+    '#9ebcda',
+    '#8c96c6',
+    '#8c6bb1',
+    '#6e016b'
+];
+
+//Declare legend variable using legend div tag
+const legend = document.getElementById('legend');
+
+//For each layer create a block to put the colour and label in
+legendlabels.forEach((label, i) => {
+    const colour = legendcolours[i];
+
+    const item = document.createElement('div'); //each layer gets a 'row' - this isn't in the legend yet, we do this later
+    const key = document.createElement('span'); //add a 'key' to the row. A key will be the colour circle
+
+    key.className = 'legend-key'; //the key will take on the shape and style properties defined in css
+    key.style.backgroundColor = colour; // the background color is retreived from teh layers array
+
+    const value = document.createElement('span'); //add a value variable to the 'row' in the legend
+    value.innerHTML = `${label}`; //give the value variable text based on the label
+
+    item.appendChild(key); //add the key (colour cirlce) to the legend row
+    item.appendChild(value); //add the value to the legend row
+
+    legend.appendChild(item); //add row to the legend
+});
